6feac0e8e30237c499c420887dbdbed2eb8aeb02
Zeromax <zero@brothers-project.de>
2013-10-19 01:06:00 +0200

    fixed code format added widget to the end of the palette

--- dca/tl_layout.php
+++ dca/tl_layout.php
@@ -13,7 +13,7 @@
 /**
  * Table tl_layout
  */
-$GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] = str_replace(',static', ',static;{usage_legend},layoutUsage,mobileLayoutUsage', $GLOBALS['TL_DCA']['tl_layout']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{usage_legend},layoutUsage,mobileLayoutUsage';
 
 $GLOBALS['TL_DCA']['tl_layout']['fields']['layoutUsage'] = array
 (
@@ -22,13 +22,12 @@ $GLOBALS['TL_DCA']['tl_layout']['fields']['layoutUsage'] = array
 	'exclude'               => true,
 	'eval'                  => array('tl_class'=>'clr', 'pickerTitle'=>$GLOBALS['TL_LANG']['tl_layout']['layoutUsage'][0],
 		'model' => array(array(
-			'table'			=>'tl_page',
-			'label'			=>'%s (%s' . $GLOBALS['TL_CONFIG']['urlSuffix'] . ')',
-			'labelValue'	=>array('title','alias'),
-			'column'		=>array('layout=? AND includeLayout=1'),
-			'value'=>array(\Input::get('id'))
+			'table'			=> 'tl_page',
+			'label'			=> '%s (%s' . $GLOBALS['TL_CONFIG']['urlSuffix'] . ')',
+			'labelValue'	=> array('title','alias'),
+			'column'		=> array('layout=? AND includeLayout=1'),
+			'value'			=> array(\Input::get('id'))
 		))
-
 	),
 	'sql'                   => "blob NULL",
 	'relation'              => array('type'=>'hasOne', 'load'=>'lazy')
@@ -40,11 +39,11 @@ $GLOBALS['TL_DCA']['tl_layout']['fields']['mobileLayoutUsage'] = array
 	'exclude'               => true,
 	'eval'                  => array('tl_class'=>'clr', 'pickerTitle'=>$GLOBALS['TL_LANG']['tl_layout']['mobileLayoutUsage'][0],
 		'model' => array(array(
-			'table'			=>'tl_page',
-			'label'			=>'%s (%s' . $GLOBALS['TL_CONFIG']['urlSuffix'] . ')',
-			'labelValue'	=>array('title','alias'),
-			'column'		=>array('mobileLayout=? AND includeLayout=1'),
-			'value'			=>array(\Input::get('id'))
+			'table'			=> 'tl_page',
+			'label'			=> '%s (%s' . $GLOBALS['TL_CONFIG']['urlSuffix'] . ')',
+			'labelValue'	=> array('title','alias'),
+			'column'		=> array('mobileLayout=? AND includeLayout=1'),
+			'value'			=> array(\Input::get('id'))
 		))
 	),
 	'sql'                     => "blob NULL",
